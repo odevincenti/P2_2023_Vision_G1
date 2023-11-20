f8a22e6fdf09fe060239f70c26cc3d213f9ac4da
odevincenti <odevincenti@itba.edu.ar>
2023-11-20 10:47:41 -0300

    Comento filter_lines

--- filter_lines.m
+++ filter_lines.m
@@ -1,15 +1,18 @@
+% filter_lines: Filtra lineas paralelas para que quede 1 por angulo
+% representativo
 function filtered_lines = filter_lines(lines)
-    filtered_lines = [];
-    for line = lines
+    filtered_lines = [];        % Inicializamos arreglo de lineas filtradas
+    for line = lines            % Iteramos sobre las lineas
         append = 1;
-        for fline = filtered_lines
+        for fline = filtered_lines    % Iteramos sobre las lineas guardadas
+            % Si los angulos son muy parecidos entre si
             if (line.theta > fline.theta - 0.1 && line.theta < fline.theta + 0.1)
-                append = 0;
+                append = 0;     % La nueva linea no sera guardada
                 break
             end
         end
-        if append == 1
-            filtered_lines = [filtered_lines, line];
+        if append == 1          % Si no encontramos similitud con otra linea
+            filtered_lines = [filtered_lines, line];    % Guardamos la linea
         end
     end
 end
