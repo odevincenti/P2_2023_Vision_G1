b791c04813a58150dc00a3f3b805726d1ac85fe1
odevincenti <odevincenti@itba.edu.ar>
2023-11-20 10:41:39 -0300

    Agrego comentarios

--- P2_2023_Vision_G1.m
+++ P2_2023_Vision_G1.m
@@ -9,94 +9,96 @@ close all
 clc
 
 %% Carga de imagen
-im=iread('resultado 20.jpg'); 
-im=idouble(im);
-imon=imono(im);
+im=iread('resultado 20.jpg');       % Cargamos la imagen
+im=idouble(im);                     % La pasamos a doble precision
+imon=imono(im);                     % La pasamos a escala de grises
 %ithresh(imon)
-idisp(im)
+%idisp(im)
 
-%% Busco centroide
-imblack=imon>0.9;         % Aplico threshold que vuelve casi todo negro
+%% Buscamos centroide
+imblack=imon>0.9;         % Aplicamos threshold que vuelve todo el dado negro
 % idisp(imblack)
-f = iblobs(imblack, 'class', 0, 'area', [10000, 30000])  % Busco blobs negros de area parecida al d20
-% f.plot_box('g')  % put a green bounding box on each blob
-% f.plot_centroid('o');  % put a circle+cross on the centroid of each blob
-% f.plot_centroid('x');
+f = iblobs(imblack, 'class', 0, 'area', [10000, 30000])  % Buscamos blobs negros de area parecida al d20
 
-% Recorto tiangulo del dado
+% Recortamos triangulo del dado
 imfoc=im(f.vmin+20:f.vmax-20, f.umin+20:f.umax-20);
-idisp(imfoc)
+%idisp(imfoc)
 
-%% Completo espacios hasta que quede sólo el triangulo
+%% Rellenamos espacios hasta que las lineas principales sean las del triangulo
 %ithresh(imfoc)
 imth=imfoc>0.28;
 imfull = iclose(imth, kcircle(3.5));
 imfull = iclose(imfull, ones(5, 5));
-idisp(imfull)
+%idisp(imfull)
 
 %% Detecto lineas
-edges = icanny(imfull);
+edges = icanny(imfull);     % Obtenemos bordes
 % idisp(edges)
+% Aplicamos transformada de Hough con 60% de votos y promedio de supresion de 5
 h = Hough(edges, 'houghthresh', 0.6, 'suppress', 5);
-lines = h.lines();
-
+lines = h.lines();          % Extraemos lineas
 % idisp(imfull, 'dark');
 
+% Filtramos las lineas que miden entre 25 y 60 ya que entre ellas se
+% encuentran los bordes del triangulo principal
 lines = lines.seglength(edges);
 k = find( lines.length > 25 & lines.length <= 60);
-
 % lines(k).plot('b--')
 % lines(k)
 
-% Suprimo lineas segun angulo
-filtered_lines = filter_lines(lines(k))
-filtered_lines.plot('b--')
+% Suprimimos lineas paralelas con un angulo similar
+filtered_lines = filter_lines(lines(k));
+% filtered_lines.plot('b--')
 
-%% Chequeo triangulos
+%% Chequeamos triangulos
+% Buscamos un triangulo equilatero entre las lineas filtradas
+% Pedimos que en sus intersecciones se formen angulos de 60°
 tri_lines = find_triangle(filtered_lines);
-idisp(imfull, 'dark');
-tri_lines.plot('b--')
-[~, idx] = sort(abs(tri_lines.theta));
+% idisp(imfull, 'dark');
+% tri_lines.plot('b--')
+[~, idx] = sort(abs(tri_lines.theta));  % Ordenamos las lineas
 tri_lines = tri_lines(idx)
-base = tri_lines(1);
-if base.rho < 60
-    base = tri_lines(2);
+base = tri_lines(1);    % Determinamos la linea mas horizontal como la base
+% Si la linea mas horizontal tiene rho menor que 60 la imagen quedara al reves al girarla
+if base.rho < 60        
+    base = tri_lines(2);    % En ese caso se toma la segunda linea mas horizontal
 end
 
 %% Corrijo orientacion
-imm=im(f.vmin:f.vmax, f.umin:f.umax);
-vc = (f.vmax - f.vmin)/2;
+imm=im(f.vmin:f.vmax, f.umin:f.umax);   % Extraemos el dado completo
+vc = (f.vmax - f.vmin)/2;               % Ubicamos su centro
 uc = (f.umax - f.umin)/2;
+% Giramos la imagen hasta que la base quede horizontal
 imrot = irotate(imm, base.theta*180/pi, 'extrapval', 1);
-idisp(imrot)
+%idisp(imrot)
 
-%% Obtengo template de los numeros a evaluar
+%% Obtenemos template de los numeros a evaluar
 numbers_template = get_numbers();
 % disp(numbers_template)
 
-%% Busco numero
+%% Buscamos numero
+% Es necesario analizar las 3 posibles orientaciones del dado
 number=0;
 max_coincidence = 0;
 
 for i=1:1:3
-    %% Corrijo orientacion
+    %% Giramos dado
     %imm=im(f.vmin:f.vmax, f.umin:f.umax);
     imrot = irotate(imrot, 120, 'extrapval', 1);
-    idisp(imrot)
+    %idisp(imrot)
     
-    %% Recorto numero
-    imfoc=imrot(vc-11:vc+11, uc-11:uc+11); % Tiene que ser 23x23
-    idisp(imfoc)
-    ithresh(imfoc)
-    % [H W] = size(imfoc)
+    %% Recortamos numero
+    imfoc=imrot(vc-11:vc+11, uc-11:uc+11);  % Tiene que ser 23x23
+    %idisp(imfoc)
+    %[H, W] = size(imfoc)
 
-    %% Aplico Threshold
+    %% Aplicamos Threshold
     imth_foc=imfoc>0.45;
     imth_foc = imth_foc*255;
-    idisp(imth_foc)
+    %idisp(imth_foc)
 
-    %%  Busco el numero, tienen que ser los dos Threshold
-    [number_aux, coincidence_aux] = find_number(numbers_template, imth_foc)
+    %% Buscamos el numero, tienen que ser los dos Threshold
+    [number_aux, coincidence_aux] = find_number(numbers_template, imth_foc);
     if coincidence_aux > max_coincidence
         number = number_aux;
         max_coincidence = coincidence_aux;
@@ -104,4 +106,5 @@ for i=1:1:3
         
 end
 
+% Resultado
 disp(['El valor del dado arrojado es: ', num2str(number)]);
