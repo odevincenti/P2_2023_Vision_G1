6f782373bee2b96e90a5e47128063624544dcbf5
odevincenti <odevincenti@itba.edu.ar>
2023-11-20 10:19:47 -0300

    Subo más ejemplos

--- blur_template.m
+++ blur_template.m
@@ -1,4 +1,4 @@
-%% Generación de ejemplos
+%% Generacion de ejemplos
 % Se aplica un kernel gaussiano a los templates para que queden ligeramente
 % difuminados (como en los ejemplos).
 % Luego se extrae y gira un dado elegido usando Paint 3D.
@@ -7,4 +7,4 @@ im=idouble(im);
 imon=imono(im);
 kgaus=kgauss(0.6);
 im=iconv(im,kgaus);
-imwrite(im, 'template_2_blur.jpg')
+imwrite(im, 'template_1_blur.jpg')
